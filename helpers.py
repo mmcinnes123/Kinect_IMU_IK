677957a469e0a2779c7a010b4d2b2715efb7dc9d
mmcinnes123 <118896958+mmcinnes123@users.noreply.github.com>
2024-05-10 15:21:04 +0100

    Started writing IK code.

--- helpers.py
+++ helpers.py
@@ -53,3 +53,36 @@ def apply_cal_to_model(thorax_virtual_IMU, humerus_virtual_IMU, radius_virtual_I
 
     model.setName("IMU_Calibrated_das")
     model.printToXML("Calibrated_" + model_file)
+
+
+def run_osim_IMU_IK(IMU_IK_settings_file, calibrated_model_file, orientations_file,
+               sensor_to_opensim_rotations, results_directory, trim_bool,
+               start_time, end_time, IK_output_file_name, visualize_tracking):
+
+    # Instantiate an InverseKinematicsTool
+    imuIK = osim.IMUInverseKinematicsTool(IMU_IK_settings_file)
+
+    # Set tool properties
+    imuIK.set_model_file(calibrated_model_file)
+    imuIK.set_orientations_file(orientations_file)
+    imuIK.set_sensor_to_opensim_rotations(sensor_to_opensim_rotations)
+    imuIK.set_results_directory(results_directory)
+    if trim_bool == True:
+        imuIK.set_time_range(0, start_time)
+        imuIK.set_time_range(1, end_time)
+    imuIK.setOutputMotionFileName(IK_output_file_name)
+
+    # Set IMU weights
+    thorax_imu_weight = osim.OrientationWeight('thorax_imu', 1.0)
+    humerus_imu_weight = osim.OrientationWeight('humerus_r_imu', 0.1)
+    radius_imu_weight = osim.OrientationWeight('radius_r_imu', 1.0)
+    print('WARNING: Humerus IMU weight set to 0.1')
+    imuIK.upd_orientation_weights().cloneAndAppend(thorax_imu_weight)
+    imuIK.upd_orientation_weights().cloneAndAppend(humerus_imu_weight)
+    imuIK.upd_orientation_weights().cloneAndAppend(radius_imu_weight)
+
+    # Run IK
+    imuIK.run(visualize_tracking)
+
+    # Update the settings .xml file
+    imuIK.printToXML(results_directory + "\\" + IMU_IK_settings_file)
--- main.py
+++ main.py
@@ -6,20 +6,30 @@ from helpers import *
 
 
 """ SETTINGS """
-# Calibration settings
-sensor_to_opensim_rotations = osim.Vec3(0, 0, 0)
-baseIMUName = 'thorax_imu'
-baseIMUHeading = '-x'  # Which axis of the thorax IMU points in same direction as the model's thorax x-axis?
-calibration_settings_template_file = "IMU_Calibration_Settings.xml"
-template_model_file = 'das3_scaled_and_placed_middle.osim'
+
 
 # Define some file paths
 parent_dir = os.getcwd()
 results_dir = os.path.join(parent_dir, 'Results')
 data_dir = os.path.join(parent_dir, 'Data')
+IMU_IK_settings_template_file = os.path.join(parent_dir, 'Settings Files', 'IMU_IK_Settings.xml')
 
-# Input orientations file
-oris_sto = 'x'
+
+# Calibration settings
+template_model_file = 'das3_scaled_and_placed_middle.osim'
+calibrated_model_file_name = 'Calibrated_das3_scaled_and_placed_middle.osim'
+
+
+# IK Settings
+oris_sto = 'x'  # Input orientations file
+sensor_to_opensim_rotations = osim.Vec3(0, 0, 0)
+baseIMUName = 'thorax_imu'
+baseIMUHeading = '-x'  # Which axis of the thorax IMU points in same direction as the model's thorax x-axis?
+trim_bool = False
+start_time = None
+end_time = None
+IK_output_file_name = os.path.join(results_dir, 'IK_Results.mot')
+visualize_tracking = False
 
 """ MAIN """
 
@@ -37,3 +47,9 @@ radius_virtual_IMU = get_IMU_cal_MANUAL('Radius')
 
 apply_cal_to_model(thorax_virtual_IMU, humerus_virtual_IMU, radius_virtual_IMU, template_model_file, results_dir)
 
+
+# Run IMU Inverse Kinematics
+
+run_osim_IMU_IK(IMU_IK_settings_template_file, calibrated_model_file_name, orientations_file,
+               sensor_to_opensim_rotations, results_dir, trim_bool,
+               start_time, end_time, IK_output_file_name, visualize_tracking)
