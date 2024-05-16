13de6f2a84c44ba5e2588f4b202c255cc055d026
mmcinnes123 <m.mcinnnes1.22@abdn.ac.uk>
2024-05-16 21:45:57 +0100

    Tidied up.

--- helpers.py
+++ helpers.py
@@ -116,21 +116,7 @@ def get_rotated_quats(quats_df):
 # This function calculates the IMU offset required which is equivalent to relying on 'manual alignment'
 # The only reason we need to apply an offset (and not just have 0 offset) is because the IMU axis names 'xyz' don't
 # match the names of the body axes, so are only rotated in multiples of 90degrees
-def get_IMU_cal_MANUAL(which_body):
 
-    if which_body == "Thorax":
-        virtual_IMU = R.from_euler('XYZ', [-90, -90, 0], degrees=True)
-
-    elif which_body == "Humerus":
-        virtual_IMU = R.from_euler('XYZ', [90, 90, 0], degrees=True)
-
-    elif which_body == "Radius":
-        virtual_IMU = R.from_euler('XYZ', [0, 0, 90], degrees=True)
-
-    else:
-        print("Which_body input wasn't 'Thorax', 'Humerus', or 'Radius'")
-
-    return virtual_IMU
 
 
 
@@ -138,7 +124,7 @@ def get_IMU_cal_MANUAL(which_body):
 
 # A function which takes an uncalibrated model (with IMUs already associated with each body)
 # and inputs an euler orientation offset which defines the virtual IMU offset relative to the bodies
-def apply_cal_to_model(thorax_virtual_IMU, humerus_virtual_IMU, radius_virtual_IMU, model_file, results_dir):
+def apply_cal_to_model(thorax_virtual_IMU, humerus_virtual_IMU, radius_virtual_IMU, model_file):
 
     model = osim.Model(model_file)
 
@@ -164,9 +150,8 @@ def apply_cal_to_model(thorax_virtual_IMU, humerus_virtual_IMU, radius_virtual_I
     model.printToXML("Calibrated_" + model_file)
 
 
-def run_osim_IMU_IK(IMU_IK_settings_file, calibrated_model_file, orientations_file,
-               sensor_to_opensim_rotations, results_directory, trim_bool,
-               start_time, end_time, IK_output_file_name, visualize_tracking):
+def run_osim_IMU_IK(IMU_IK_settings_file, calibrated_model_file, orientations_file, results_directory, trim_bool,
+               start_time, end_time, IK_output_file_name):
 
     # Instantiate an InverseKinematicsTool
     imuIK = osim.IMUInverseKinematicsTool(IMU_IK_settings_file)
@@ -174,12 +159,12 @@ def run_osim_IMU_IK(IMU_IK_settings_file, calibrated_model_file, orientations_fi
     # Set tool properties
     imuIK.set_model_file(calibrated_model_file)
     imuIK.set_orientations_file(orientations_file)
-    imuIK.set_sensor_to_opensim_rotations(sensor_to_opensim_rotations)
+    imuIK.set_sensor_to_opensim_rotations(osim.Vec3(0, 0, 0))
     imuIK.set_results_directory(results_directory)
+    imuIK.setOutputMotionFileName(IK_output_file_name)
     if trim_bool == True:
         imuIK.set_time_range(0, start_time)
         imuIK.set_time_range(1, end_time)
-    imuIK.setOutputMotionFileName(IK_output_file_name)
 
     # Set IMU weights
     thorax_imu_weight = osim.OrientationWeight('thorax_imu', 1.0)
@@ -190,7 +175,5 @@ def run_osim_IMU_IK(IMU_IK_settings_file, calibrated_model_file, orientations_fi
     imuIK.upd_orientation_weights().cloneAndAppend(radius_imu_weight)
 
     # Run IK
-    imuIK.run(visualize_tracking)
+    imuIK.run(False)
 
-    # # Save the settings .xml file for reference
-    # imuIK.printToXML(results_directory + "\\" + IMU_IK_settings_file)
